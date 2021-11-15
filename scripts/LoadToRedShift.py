6c5d793c3e74a996160a706418d87684fd0be538
tariq <tariq.saeed81@gmail.com>
2021-11-15 21:39:05 +0200

    documentation

--- scripts/LoadToRedShift.py
+++ scripts/LoadToRedShift.py
@@ -22,22 +22,61 @@ def createConCurObjects():
     return (conn,cur)
 
 def createSchema(conn,cur):
+    """
+        Description: This function creates the schema used in the model
+
+        Arguments:
+            conn: The connection object
+            cur: The cursor object
+
+        Returns:
+            None
+    """
     cur.execute(create_schema)
     conn.commit()
 
 def createTables(conn,cur):
+    """
+        Description: This function creates the tables used in the model.
+
+        Arguments:
+            conn: The connection object
+            cur: The cursor object
+
+        Returns:
+            None
+    """
     for query in create_tables_list:
         cur.execute(query)
         conn.commit()
 
 def purgeTables(conn,cur):
+    """
+        Description: This function purges all the tables in the model.
+
+        Arguments:
+            conn: The connection object
+            cur: The cursor object
+
+        Returns:
+            None
+    """
     for tblName in config["DWH_TABLES"]:
         query = delete_table_sql.format(config["DWH_SCHEMA"]["MODEL_SCH"],config["DWH_TABLES"][tblName])
         cur.execute(query)
         conn.commit()
 
 def copyTables(conn,cur):
+    """
+        Description: This function copies the data from S3 to the target table in Redshift
 
+        Arguments:
+            conn: The connection object
+            cur: The cursor object
+
+        Returns:
+            None
+    """
     for i in range(0,len(src_dl_tables_list)):
         query = copy_table_sql.format(trgt_dw_tables_list[i],config["S3"]["TARGET_BUCKET"],
                                       src_dl_tables_list[i],config["S3"]["AWS_ACCESS_KEY_ID"],
